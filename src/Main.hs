7bfa500fa048daf15180110c3d01b954c9831d02
Joshua Barrass <josh.barrass.work@gmail.com>
2024-11-14 23:32:16 +0000

    New Main

--- src/Main.hs
+++ src/Main.hs
@@ -1,19 +1,25 @@
 module Main where
 
-import Goods
-import Sector
+import IniParse
+import SectorMap ( getSector )
+import Sector (distanceIndex)
+import Goods (getGoodByID)
+import System.Exit (exitFailure)
+import System.Environment
 
-g1 = Good 1 "Wood" 19
-g2 = Good 2 "Food" 25
+exitOnNothing :: Maybe a -> IO a
+exitOnNothing Nothing = exitFailure
+exitOnNothing (Just x) = return x
 
-s1 = Sector 1 (Just Wall) (Just Wall) (Just Wall) (Just s2) 1 [] [g1, g2]
-s2 = Sector 2 (Just Wall) (Just Wall) (Just s1) (Just s3) 1 [g1] []
-s3 = Sector 3 (Just Wall) (Just Wall) (Just s2) (Just Wall) 1 [g2] []
+goodType = 8
 
 main :: IO ()
 main = do
-  let d1 = distanceIndex g1 s1
-  let d2 = distanceIndex g2 s1
-  print d1
-  print d2
-
+  (filename:_) <- getArgs
+  f <- readFile filename
+  ini <- exitOnNothing $ parseIni f
+  sec <- exitOnNothing $ getSector ini 60
+  print sec
+  putStr $ "Distance index for Good " ++ show goodType ++ ": "
+  distanceIndex <- exitOnNothing $ distanceIndex (getGoodByID goodType) sec
+  print distanceIndex
