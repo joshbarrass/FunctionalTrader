57eea3e6b1540a639595b33ff78cf43de1e55698
Joshua Barrass <josh.barrass.work@gmail.com>
2024-11-14 23:07:43 +0000

    Parse goods for the sector

--- src/SectorMap.hs
+++ src/SectorMap.hs
@@ -7,7 +7,8 @@ import IniParse
 import qualified Data.Map as Map
 -- import Data.Text (isInfixOf)
 import Data.List (isInfixOf)
-import Sector
+import Sector ( Sector(Sector, Wall) )
+import Goods
 
 getSectorKeys :: Ini -> [String]
 getSectorKeys ini = filter (isInfixOf "Sector=") (Map.keys ini)
@@ -21,8 +22,28 @@ getSector ini n = do
   let l = handleMaybeDir (Map.lookup "Left" sector)
   let r = handleMaybeDir (Map.lookup "Right" sector)
 
-  return $ Sector n u d l r 0 [] []
+  return $ Sector n u d l r 0 (parseBuys sector) (parseSells sector)
 
   where handleMaybeDir :: Maybe String -> Maybe Sector
         handleMaybeDir (Just s) = getSector ini (read s)
         handleMaybeDir Nothing = Just Wall
+
+splitByComma :: String -> [String]
+splitByComma "" = []
+splitByComma s = case break (== ',') s of
+    (before, "")     -> [before]
+    (before, _:rest) -> before : splitByComma rest
+
+
+parseGoods :: String -> [Good]
+parseGoods = map (getGoodByID . read) . splitByComma
+
+parseBuys :: Map.Map String String -> [Good]
+parseBuys sector
+  | "Buys" `Map.member` sector = parseGoods (sector Map.! "Buys")
+  | otherwise = []
+
+parseSells :: Map.Map String String -> [Good]
+parseSells sector
+  | "Sells" `Map.member` sector = parseGoods (sector Map.! "Sells")
+  | otherwise = []
