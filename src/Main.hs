85f69fead22c97778fa5802416f9e1855cae6d60
Joshua Barrass <josh.barrass.work@gmail.com>
2024-11-15 23:15:40 +0000

    Very crude optimal two-way route calculator
    Currently doesn't have very satisfying printing, and currently doesn't
    take relations into account.

--- src/Main.hs
+++ src/Main.hs
@@ -8,7 +8,7 @@ import Trading
 import System.Exit (exitFailure)
 import System.Environment
 import Data.Foldable (for_)
-import Data.Maybe (isNothing, fromJust)
+import Data.Maybe (isNothing, isJust, fromJust)
 import Data.Ord
 import Data.List
 
@@ -26,7 +26,7 @@ getAllJust [Nothing] = []
 getAllJust [Just x] = [x]
 getAllJust (x:xs) = (getAllJust [x]) ++ (getAllJust xs)
 
-allPairs :: (Eq a) => [a] -> [a] -> [(a,a)]
+allPairs :: [a] -> [b] -> [(a,b)]
 allPairs xs ys = [(x,y) | x <-xs, y<-ys]
 
 printRouteProfitability :: (Fractional a, Show a) => ((Sector, Sector), a) -> IO ()
@@ -48,14 +48,20 @@ main = do
   let creontiSecs = getAllJust $ map (getSector ini) [50..95]
   let goodSellers = filter (`doesSell` theGood) creontiSecs
   let goodBuyers = filter (`doesBuy` theGood) creontiSecs
-  let buySellPairs = allPairs goodSellers goodBuyers
-  let profits = getAllJust $ map (\(buyFrom, sellTo) -> calculateProfitPerTurn buyFrom sellTo theGood) buySellPairs
-  let ordered = reverse $ sortBy (comparing snd) (zip buySellPairs profits)
+  let portPairs = filter (\(port1, port2) -> (num port1) < (num port2)) $ allPairs goodSellers goodBuyers
+  let goodPairs = allPairs allGoods allGoods
+  let allCombos = allPairs portPairs goodPairs
+  
+  let profits = map (\((port1, port2), (good1, good2)) -> calculateTwoWayProfitPerTurn port1 port2 good1 good2) allCombos
+  let ordered = reverse $ sortBy (comparing (fromJust . snd)) $ filter (isJust . snd) (zip allCombos profits)
   let best = head ordered
-  putStr "Most profitable route for "
-  putStr . show $ name theGood
-  putStr " is "
-  printRouteProfitability best
+  putStrLn . show . snd $ best
+  putStrLn . show . fst $ best
+  putStrLn . show . fromJust $ calculateTwoWayProfitPerTurn (fromJust $ getSector ini 87) (fromJust $ getSector ini 74) (getGoodByID 8) (getGoodByID 12)
+  -- putStr "Most profitable route for "
+  -- putStr . show $ name theGood
+  -- putStr " is "
+  -- printRouteProfitability best
 
-  putStrLn "\nOther options:"
-  for_ (tail ordered) printRouteProfitability 
+  -- putStrLn "\nOther options:"
+  -- for_ (tail ordered) printRouteProfitability 
