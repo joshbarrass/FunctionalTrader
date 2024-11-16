118bec60fc58915c88868bc1d5eeb559c04f8c49
Joshua Barrass <josh.barrass.work@gmail.com>
2024-11-16 00:21:20 +0000

    Include relations in route profitability calculations
    This can be an important factor; if a route would trade between
    e.g. your own race and the neutral race, it may be less profitable
    than a route between two ports both of your race.

--- src/Trading.hs
+++ src/Trading.hs
@@ -10,44 +10,60 @@ module Trading (
 -- import Math
 import Sector
 import Goods ( Good(price) )
+import Data.Maybe
+import qualified Data.Map as Map
+
+type Relations = Map.Map Integer Integer
+
+getRelation :: Relations -> Integer -> Integer
+getRelation rels race = fromMaybe 0 rel
+  where rel = Map.lookup race rels
+
+getRelFactor :: Integer -> Double
+getRelFactor rel = if factor > 1 then 1 else factor
+  where factor = fromIntegral rel / 1000.0
 
 -- the lowest price a sector will accept for a player buying this good
 -- from them
-buyPrice :: Sector -> Good -> Maybe Integer
-buyPrice sec good = do
+buyPrice :: Relations -> Sector -> Good -> Maybe Integer
+buyPrice rels sec good = do
   distanceIndex <- sellDistanceIndex sec good
-  return $ ceiling $ 0.03 * (fromIntegral $ price good) * ((fromIntegral distanceIndex)**1.3) * (2 - 1) * (3)
+  return $ ceiling $ 0.03 * (fromIntegral $ price good) * ((fromIntegral distanceIndex)**1.3) * (2 - 1) * (3 - 2*relFactor)
+  where rel = getRelation rels (race sec)
+        relFactor = getRelFactor rel
 
 -- the highest price a sector will accept for a player selling this
 -- good to them
-sellPrice :: Sector -> Good -> Maybe Integer
-sellPrice sec good = do
+sellPrice :: Relations -> Sector -> Good -> Maybe Integer
+sellPrice rels sec good = do
   distanceIndex <- buyDistanceIndex sec good
-  return $ floor $ 0.088 * (fromIntegral $ price good) * ((fromIntegral distanceIndex)**1.3) * (1 + 1) * (1.2)
+  return $ floor $ 0.088 * (fromIntegral $ price good) * ((fromIntegral distanceIndex)**1.3) * (1 + 1) * (1.2 + 1.8*relFactor)
+  where rel = getRelation rels (race sec)
+        relFactor = getRelFactor rel
 
-calculateProfit :: Sector -> Sector -> Good -> Maybe Integer
-calculateProfit buyFrom sellTo good
+calculateProfit :: Relations -> Sector -> Sector -> Good -> Maybe Integer
+calculateProfit rels buyFrom sellTo good
   | not (buyFrom `doesSell` good) = Nothing
   | not (sellTo `doesBuy` good) = Nothing
   | otherwise = do
-    buyFor <- buyPrice buyFrom good
-    sellFor <- sellPrice sellTo good
+    buyFor <- buyPrice rels buyFrom good
+    sellFor <- sellPrice rels sellTo good
     return $ sellFor - buyFor
 
-calculateProfitPerTurn :: (Fractional a) => Sector -> Sector -> Good -> Maybe a
-calculateProfitPerTurn buyFrom sellTo good = do
-  profit <- calculateProfit buyFrom sellTo good
+calculateProfitPerTurn :: (Fractional a) => Relations -> Sector -> Sector -> Good -> Maybe a
+calculateProfitPerTurn rels buyFrom sellTo good = do
+  profit <- calculateProfit rels buyFrom sellTo good
   distance <- buyFrom `distanceTo` ((num sellTo ==) . num)
   return $ fromIntegral profit / fromIntegral (distance + 2)
 
-calculateTwoWayProfit :: Sector -> Sector -> Good -> Good -> Maybe Integer
-calculateTwoWayProfit port1 port2 sellAt2 sellAt1 = do
-  way1 <- calculateProfit port1 port2 sellAt2
-  way2 <- calculateProfit port2 port1 sellAt1
+calculateTwoWayProfit :: Relations -> Sector -> Sector -> Good -> Good -> Maybe Integer
+calculateTwoWayProfit rels port1 port2 sellAt2 sellAt1 = do
+  way1 <- calculateProfit rels port1 port2 sellAt2
+  way2 <- calculateProfit rels port2 port1 sellAt1
   return $ way1 + way2
 
-calculateTwoWayProfitPerTurn :: (Fractional a) => Sector -> Sector -> Good -> Good -> Maybe a
-calculateTwoWayProfitPerTurn port1 port2 sellAt2 sellAt1 = do
-  profit <- calculateTwoWayProfit port1 port2 sellAt2 sellAt1
+calculateTwoWayProfitPerTurn :: (Fractional a) => Relations -> Sector -> Sector -> Good -> Good -> Maybe a
+calculateTwoWayProfitPerTurn rels port1 port2 sellAt2 sellAt1 = do
+  profit <- calculateTwoWayProfit rels port1 port2 sellAt2 sellAt1
   distance <- port1 `distanceTo` ((num port2 ==) . num)
   return $ fromIntegral profit / fromIntegral (2*distance + 4)
