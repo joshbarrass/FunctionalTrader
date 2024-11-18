80646f4c6db2f58bbb0b33f8f0989390e34bd87c
Joshua Barrass <josh.barrass.work@gmail.com>
2024-11-18 00:10:50 +0000

    Modify profit calculations to be the expected value including fines

--- src/Trading.hs
+++ src/Trading.hs
@@ -45,22 +45,26 @@ sellPrice rels sec good = do
   where rel = getRelation rels (race sec)
         relFactor = getRelFactor rel
 
-calculateProfit :: Relations -> Sector -> Sector -> Good -> Maybe Integer
+calculateProfit :: (Fractional a) => Relations -> Sector -> Sector -> Good -> Maybe a
 calculateProfit rels buyFrom sellTo good
   | not (buyFrom `doesSell` good) = Nothing
   | not (sellTo `doesBuy` good) = Nothing
   | otherwise = do
     buyFor <- buyPrice rels buyFrom good
     sellFor <- sellPrice rels sellTo good
-    return $ sellFor - buyFor
+    let fc = fineChance good sellTo
+    let nfc = 1 - fc
+    let profit = sellFor - buyFor
+    let expected = (nfc * fromIntegral profit) - (fc * fromIntegral (calculateFine good sellTo + buyFor))
+    return expected
 
 calculateProfitPerTurn :: (Fractional a) => Relations -> Sector -> Sector -> Good -> Maybe a
 calculateProfitPerTurn rels buyFrom sellTo good = do
   profit <- calculateProfit rels buyFrom sellTo good
   distance <- buyFrom `distanceTo` ((num sellTo ==) . num)
-  return $ fromIntegral profit / fromIntegral (distance + 2)
+  return $ profit / fromIntegral (distance + 2)
 
-calculateTwoWayProfit :: Relations -> Sector -> Sector -> Good -> Good -> Maybe Integer
+calculateTwoWayProfit :: (Fractional a) => Relations -> Sector -> Sector -> Good -> Good -> Maybe a
 calculateTwoWayProfit rels port1 port2 sellAt2 sellAt1 = do
   way1 <- calculateProfit rels port1 port2 sellAt2
   way2 <- calculateProfit rels port2 port1 sellAt1
@@ -70,14 +74,19 @@ calculateTwoWayProfitPerTurn :: (Fractional a) => Relations -> Sector -> Sector 
 calculateTwoWayProfitPerTurn rels port1 port2 sellAt2 sellAt1 = do
   profit <- calculateTwoWayProfit rels port1 port2 sellAt2 sellAt1
   distance <- port1 `distanceTo` ((num port2 ==) . num)
-  return $ fromIntegral profit / fromIntegral (2*distance + 4)
+  return $ profit / fromIntegral (2*distance + 4)
 
 -- per good
 calculateFine :: Good -> Sector -> Integer
 calculateFine good sector = price good * level sector
 
-calculateInspectionChance :: Sector -> Double
+calculateInspectionChance :: (Fractional a) => Sector -> a
 calculateInspectionChance sector = baseChance - reductionPerIllegal * numIllegal
   where numIllegal = fromIntegral $ length (filter illegal (sells sector)) + length (filter illegal (buys sector))
         baseChance = 0.15
         reductionPerIllegal = 0.04
+
+fineChance :: (Fractional a) => Good -> Sector -> a
+fineChance good sec
+  | illegal good = calculateInspectionChance sec
+  | otherwise = 0.0
